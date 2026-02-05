48c7bcd54f5e3fa9dd06fcbe6202a9c841c7afe3
dongc <dongc@LAPTOP-QC1KLDBD>
2026-02-05 15:59:03 -0800

    Add project files.

---
+++ WebsiteStatus/Worker.cs
@@ -0,0 +1,54 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebsiteStatus
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private HttpClient client;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            client = new HttpClient();
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            client.Dispose();
+            _logger.LogInformation("The service has been stoped...");
+            return base.StopAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                var result = await client.GetAsync("https://www.google.com");
+
+                if(result.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation("Hello, the website is up. Status code is {StatusCode}", result.StatusCode);
+                }
+                else
+                {
+                    _logger.LogError("The website is down. Status code {StatusCode}", result.StatusCode);
+                }
+                await Task.Delay(5*1000, stoppingToken);
+            }
+        }
+    }
+}
